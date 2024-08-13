693eecd417c9975adcf0d1c4873cc4433fb2e4d6
Nas2312 <nasta.romanenko.03@mail.ru>
2024-08-13 22:08:58 +0700

    Регистрация

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+include 'includes/db.php';
+include 'includes/auth.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->execute([$username]);
+    $user = $stmt->fetch();
+
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['is_admin'] = $user['is_admin'];
+        header('Location: index.php');
+    } else {
+        echo "Invalid credentials.";
+    }
+}
+?>
+
+<form method="post">
+    Username: <input type="text" name="username"><br>
+    Password: <input type="password" name="password"><br>
+    <input type="submit" value="Login">
+</form>
